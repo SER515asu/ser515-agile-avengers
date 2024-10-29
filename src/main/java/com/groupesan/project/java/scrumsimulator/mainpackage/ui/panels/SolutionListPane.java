96579fc9a438c08c747b237ca754bb98cbcf6481
Swathi Anaji Revanasiddappa <srevanas@asu.edu>
2024-10-29 10:31:37 -0700

    Added UI to show list of added solutions

--- src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/SolutionListPane.java
+++ src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/panels/SolutionListPane.java
@@ -1,7 +1,10 @@
 package com.groupesan.project.java.scrumsimulator.mainpackage.ui.panels;
 
 import com.groupesan.project.java.scrumsimulator.mainpackage.core.Player;
+import com.groupesan.project.java.scrumsimulator.mainpackage.impl.Solution;
+import com.groupesan.project.java.scrumsimulator.mainpackage.impl.SolutionStore;
 import com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets.BaseComponent;
+import com.groupesan.project.java.scrumsimulator.mainpackage.ui.widgets.SolutionWidget;
 import com.groupesan.project.java.scrumsimulator.mainpackage.utils.CustomConstraints;
 
 import javax.swing.*;
@@ -9,6 +12,8 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +24,7 @@ public class SolutionListPane extends JFrame implements BaseComponent {
         this.player = player;
         this.init();
     }
-//    private List<UserStoryWidget> widgets = new ArrayList<>();
+    private List<SolutionWidget> widgets = new ArrayList<>();
     private JPanel subPanel = new JPanel();
 
     public void init(){
@@ -34,8 +39,13 @@ public class SolutionListPane extends JFrame implements BaseComponent {
 
         subPanel.setLayout(new GridBagLayout());
 
+        JScrollPane scrollPane = new JScrollPane(subPanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        refreshSolutionList();
+
         myJpanel.add(
-                new JScrollPane(subPanel),
+                scrollPane,
                 new CustomConstraints(0, 0, GridBagConstraints.WEST, 1.0, 0.8, GridBagConstraints.BOTH)
         );
 
@@ -46,6 +56,14 @@ public class SolutionListPane extends JFrame implements BaseComponent {
                     public void actionPerformed(ActionEvent e) {
                         SolutionForm form = new SolutionForm();
                         form.setVisible(true);
+                        form.addWindowListener(
+                                new WindowAdapter() {
+                                    @Override
+                                    public void windowClosed(WindowEvent e) {
+                                        refreshSolutionList();
+                                    }
+                                }
+                        );
                     }
                 }
         );
@@ -56,5 +74,28 @@ public class SolutionListPane extends JFrame implements BaseComponent {
         );
 
         add(myJpanel);
+    }
+
+    private void refreshSolutionList(){
+        subPanel.removeAll();
+        widgets.clear();
+
+        List<Solution> solutions = SolutionStore.getInstance().getSolutions();
+        for(int i=0; i<solutions.size(); i++){
+            SolutionWidget widget = new SolutionWidget(solutions.get(i));
+            widgets.add(widget);
+            subPanel.add(
+                    widget,
+                    new CustomConstraints(
+                            0,
+                            i+1,
+                            GridBagConstraints.WEST,
+                            1.0,
+                            0.1,
+                            GridBagConstraints.HORIZONTAL));
+        }
+
+        subPanel.revalidate();
+        subPanel.repaint();
     }
 }
